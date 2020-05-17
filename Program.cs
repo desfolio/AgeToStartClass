c84701f1e7445785cec876d2ad6600b18957c6e0
Desmond Williams <desmond@desfolio.com>
2020-05-16 21:44:33 -0400

    met all requirements

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace AgeToStartClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            var isValidAge = int.TryParse(Console.ReadLine(), out int age);
+
+            const int legalAge = 21;
+
+            if (isValidAge)
+            {
+                if ((firstName.ToLower() == "tim" || firstName.ToLower() == "sue")
+                        && age >= legalAge)
+                {
+                    Console.WriteLine($"Hello Professor {firstName}. Welcome to the class.");
+                }
+                else if ((firstName.ToLower() == "tim" || firstName.ToLower() == "sue")
+                        && age < legalAge)
+                {
+                    Console.WriteLine($"Hello Professor {firstName}. We recommend you wait " +
+                        $"{legalAge - age} years to start this class.");
+                }
+                else if (age < legalAge)
+                {
+                    Console.WriteLine($"Hello {firstName}. We recommend you wait " +
+                        $"{legalAge - age} years to start this class.");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello {firstName}. Welcome to the class.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Did not enter a valid age!");
+            }
         }
     }
 }
